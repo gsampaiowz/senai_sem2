42771e49c3c80bc50344287f01629774c5acb335
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-08-04 17:17:24 -0300

    DQL Quase finalizado

--- ddl_dml_dql/Exercicios/1_4/DQL.sql
+++ ddl_dml_dql/Exercicios/1_4/DQL.sql
@@ -1,55 +1,36 @@
--- listar todos os veterinários (nome e CRMV) de uma clínica (razão social)
--- listar todas as raças que começam com a letra S
--- listar todos os tipos de pet que terminam com a letra O
--- listar todos os pets mostrando os nomes dos seus donos
--- listar todos os atendimentos mostrando o nome do veterinário que atendeu, o nome, a raça e o tipo do pet que foi atendido, o nome do dono do pet e o nome da clínica onde o pet foi atendido
+-- listar todos os usuários administradores, sem exibir suas senhas
+-- listar todos os álbuns lançados após o um determinado ano de lançamento
+-- listar os dados de um usuário através do e-mail e senha
+-- listar todos os álbuns ativos, mostrando o nome do artista e os estilos do álbum 
+
+SELECT 
+	Nome, Email, IdPermissao
+FROM	
+	Usuarios
 
 SELECT
-	V.Nome
+	*
 FROM
-	Veterinario AS V
+	Albuns
 WHERE
-	V.IdClinica = 2
-
---SELECT NOS REGISTROS
-SELECT
-	R.Descricao
-FROM
-	Raca AS R
-WHERE
-	RIGHT(R.Descricao,1) = 'o'
+	AnoLancamento > 2020
 
 SELECT
-	D.Nome,
-	P.Nome
+	*
 FROM
-	Dono AS D,
-	Pet AS P
+	Usuarios
 WHERE
-	P.IdDono = D.IdDono
+	Email = 'meci@email.com'
+	AND Senha = 123
 
-
-SELECT
-	A.IdAtendimento,
-	V.Nome AS NomeVet,
-	P.Nome AS NomePet,
-	R.Descricao AS Raca,
-	T.Descricao AS Tipo,
-	D.Nome AS NomeDono,
-	C.Endereco
-FROM
-	Atendimento AS A,
-	Veterinario AS V,
-	Pet AS P,
-	Raca AS R,
-	TipoPet AS T,
-	Dono AS D,
-	Clinica AS C
+SELECT	
+	Artistas.Nome AS Artista,
+	Estilos.Nome AS Estilo,
+	Albuns.Titulo AS Titulo
+FROM Albuns
+	inner join AlbunsEstilos on Albuns.IdAlbum = AlbunsEstilos.IdAlbum join AlbunsEstilos on Estilos.IdEstilo = AlbunsEstilos.IdEstilo
+	inner join Artistas on Albuns.IdArtista = Artistas.IdArtista
 WHERE
-	P.IdTipoPet = T.IdTipoPet
-	AND P.IdRaca = R.IdRaca
-	AND P.IdDono = D.IdDono
-	AND R.IdTipoPet = T.IdTipoPet
-	AND V.IdClinica = C.IdClinica
-	AND A.IdVeterinario = V.IdVeterinario
-	AND P.IdPet = A.IdPet
+	Albuns.IdAtivo = 1
+
+	
