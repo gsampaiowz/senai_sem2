3f249cdc9760ae9f82e479f74fd107d3f95a5b2f
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-09-12 17:25:29 -0300

    Database first iniciado

---
+++ webapi.inlock.tarde/Controllers/EstudioController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.inlock.tarde.Interfaces;
+using webapi.inlock.tarde.Repositories;
+
+namespace webapi.inlock.tarde.Controllers
+    {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+
+    public class EstudioController : ControllerBase
+        {
+        private IEstudioRepository _estudioRepository { get; set; }
+
+        public EstudioController()
+            {
+            _estudioRepository = new EstudioRepository();
+            }
+
+        [HttpGet]
+        public IActionResult GetAll()
+            {
+            try
+                {
+                return Ok(_estudioRepository.ListarTodos());
+                }
+            catch (Exception erro)
+                {
+
+                throw new Exception("Erro ao listar os estúdios" + erro.Message);
+                }
+            }
+
+        [HttpGet("ListarComJogos")]
+        public IActionResult GetWithGames()
+            {
+            try
+                {
+                return Ok(_estudioRepository.ListarComJogos());
+                }
+            catch (Exception erro)
+                {
+
+                throw new Exception("Erro ao listar os estúdios com os jogos" + erro.Message);
+                }
+            }
+        }
+    }
+
---
+++ webapi.inlock.tarde/Interfaces/IEstudioRepository.cs
@@ -0,0 +1,14 @@
+﻿using webapi.inlock.tarde.Domains;
+
+namespace webapi.inlock.tarde.Interfaces
+    {
+    public interface IEstudioRepository
+        {
+        List<Estudio> ListarTodos();
+        Estudio BuscarPorId(Guid id);
+        void Cadastrar(Estudio novoEstudio);
+        void Atualizar(Guid id, Estudio estudioAtualizado_);
+        void Deletar(Guid id);
+        List<Estudio> ListarComJogos();
+        }
+    }
