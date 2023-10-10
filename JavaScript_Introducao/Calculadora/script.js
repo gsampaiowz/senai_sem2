14e34b8710a2b0ddd24706980a8d5dcd13f00bf9
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-10-10 17:25:18 -0300

    Continuada aula de Javascript

--- JavaScript_Introducao/Calculadora/script.js
+++ JavaScript_Introducao/Calculadora/script.js
@@ -5,27 +5,31 @@ function Calcular() {
 
 	let n2 = parseFloat(document.getElementById("n2").value);
 
+	
 	let result = document.getElementById("result").innerHTML;
 
 	let op = document.getElementById("op").value;
 
 	switch (op) {
 		case "soma":
-			result = `Resultado: ${n1 + n2}`;
+			result = `Resultado: ${(n1 + n2).toFixed(2)}`;
 			break;
 		case "sub":
-			result = `Resultado: ${n1 - n2}`;
+			result = `Resultado: ${(n1 - n2).toFixed(2)}`;
 			break;
 		case "mult":
-			result = `Resultado: ${n1 * n2}`;
+			result = `Resultado: ${(n1 * n2).toFixed(2)}`;
 			break;
 		case "div":
-			result = `Resultado: ${n1 / n2}`;
+			result = `Resultado: ${(n1 / n2).toFixed(2)}`;
+			if (n2 == 0) result = "Não é possível dividir por 0!";
 			break;
 		default:
-			result = 'Selecione uma operação!';
+			result = "Selecione uma operação!";
 			break;
 	}
+
+	if (isNaN(n1) || isNaN(n2)) result = "Preencha todos os campos!";
 
 	document.getElementById("result").innerHTML = result;
 }
