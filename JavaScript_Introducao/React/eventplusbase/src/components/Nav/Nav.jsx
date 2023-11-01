8abc2a7480d1288f47f0e5c61355ef00646761eb
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-11-01 17:24:04 -0300

    hotfix

--- JavaScript_Introducao/React/eventplusbase/src/components/Nav/Nav.jsx
+++ JavaScript_Introducao/React/eventplusbase/src/components/Nav/Nav.jsx
@@ -10,13 +10,27 @@ const Nav = () => {
     <nav className="navbar">
       <span className="navbar__close">x</span>
 
-      <Link to="/"><img className="eventlogo__logo-image" src={window.innerWidth ? logoDesktop: logoMobile} alt="Event Plus Logo" /></Link>
+      <Link to="/">
+        <img
+          className="eventlogo__logo-image"
+          src={window.innerWidth > 720 ? logoDesktop : logoMobile}
+          alt="Event Plus Logo"
+        />
+      </Link>
 
       <div className="navbar__items-box">
-        <Link to="/" className="navbar__item">Home</Link>
-        <Link to="/eventos" className="navbar__item">Eventos</Link>
-        <Link to="/tipo-eventos" className="navbar__item">Tipos de eventos</Link>
-        <Link to="/login" className="navbar__item">Login</Link>
+        <Link to="/" className="navbar__item">
+          Home
+        </Link>
+        <Link to="/eventos" className="navbar__item">
+          Eventos
+        </Link>
+        <Link to="/tipo-eventos" className="navbar__item">
+          Tipos de eventos
+        </Link>
+        <Link to="/login" className="navbar__item">
+          Login
+        </Link>
       </div>
     </nav>
   );
