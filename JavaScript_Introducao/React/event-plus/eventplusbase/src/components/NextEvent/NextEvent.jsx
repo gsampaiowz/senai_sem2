992b2bd780a07ea94a5f938f80a6c8f00d193b5a
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-11-10 15:37:57 -0300

    Data e descricão formatadas, EventPlus projeto

--- JavaScript_Introducao/React/event-plus/eventplusbase/src/components/NextEvent/NextEvent.jsx
+++ JavaScript_Introducao/React/event-plus/eventplusbase/src/components/NextEvent/NextEvent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NextEvent.css";
+import dateFormatDbToView from "../../Utils/stringFunction";
 
 const NextEvent = ({ title, description, eventDate, idEvento }) => {
   function conectar(idEvento) {
@@ -10,8 +11,8 @@ const NextEvent = ({ title, description, eventDate, idEvento }) => {
     <article className="event-card">
       <h2 className="event-card__title">{title}</h2>
 
-      <p className="event-card__description">{description}</p>
-      <p className="event-card__description">{eventDate}</p>
+      <p className="event-card__description">{description.substr(0,16)}</p>
+      <p className="event-card__description">{dateFormatDbToView(eventDate)}</p>
 
       <a
         href="/"
