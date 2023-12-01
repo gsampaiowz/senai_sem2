638e6beff4559454883719c24b253d1bb3d7274b
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-12-01 15:29:25 -0300

    Authorização ternária

--- JavaScript_Introducao/React/event-plus/eventplusbase/src/context/AuthContext.js
+++ JavaScript_Introducao/React/event-plus/eventplusbase/src/context/AuthContext.js
@@ -7,7 +7,7 @@ export const userDecodeToken = (theToken) => {
   const decoded = jwtDecode(theToken); //aqui retorna o payload do token
 
   return {
-    perfil: decoded.role,
+    role: decoded.role,
     name: decoded.name,
     userId: decoded.jti,
     token: theToken,
