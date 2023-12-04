3ca48c71547f2f8549a9e3dc7a0cbc420805c6ff
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-12-04 17:19:54 -0300

    Eventos Aluno listagem finalizada

---
+++ JavaScript_Introducao/React/event-plus/eventplusbase/src/pages/EventosAlunoPage/EventosAlunoPage.jsx
@@ -0,0 +1,134 @@
+import React, { useContext, useEffect, useState } from "react";
+import MainContent from "../../components/MainContent/MainContent";
+import Title from "../../components/Title/Title";
+import Table from "./TableEva/Table";
+import Container from "../../components/Container/Container";
+import { Select } from "../../components/FormComponents/FormComponents";
+import Spinner from "../../components/Spinner/Spinner";
+import Modal from "../../components/Modal/Modal";
+import api from "../../Services/Service";
+
+import "./EventosAlunoPage.css";
+import { userContext } from "../../context/AuthContext";
+
+const EventosAlunoPage = () => {
+  const [eventos, setEventos] = useState([]);
+  const [meusEventos, setMeusEventos] = useState([]);
+  // select mocado
+  const [quaisEventos, setQuaisEventos] = useState([
+    { value: "1", text: "Todos os eventos" },
+    { value: "2", text: "Meus eventos" },
+  ]);
+
+  const [tipoEvento, setTipoEvento] = useState("1"); //código do tipo do Evento escolhido
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  // recupera os dados globais do usuário
+  const { userData, setUserData } = useContext(userContext);
+
+  //METODO PARA LISTAR EVENTOS
+  async function getEventos() {
+    setShowSpinner(true);
+    try {
+      const promise = await api.get("/Evento");
+      setEventos(promise.data);
+    } catch (error) {
+      console.error("Erro ao carregar eventos: " + error);
+    }
+    setShowSpinner(false);
+  }
+
+  async function getMeusEventos() {
+    setShowSpinner(true);
+    try {
+      const promise = await api.get(
+        `/PresencasEvento/ListarMinhas/${userData.userId}`
+      );
+
+      let novosEventos = [...meusEventos];
+      promise.data.forEach((e) => {
+        novosEventos.push(e.evento);
+      });
+      setMeusEventos(novosEventos);
+      console.log(meusEventos);
+    } catch (error) {
+      console.error("Erro ao carregar eventos: " + error);
+    }
+    setShowSpinner(false);
+  }
+
+  //LISTAGEM DE EVENTOS
+  useEffect(() => {
+    //chamar a api
+    getEventos();
+    getMeusEventos();
+  }, []);
+
+  // toggle meus eventos ou todos os eventos
+  function myEvents(tpEvent) {
+    setTipoEvento(tpEvent);
+  }
+
+  async function loadMyComentary(idComentary) {
+    return "????";
+  }
+
+  const showHideModal = () => {
+    setShowModal(showModal ? false : true);
+  };
+
+  const commentaryRemove = () => {
+    alert("Remover o comentário");
+  };
+
+  function handleConnect() {
+    alert("Desenvolver a função conectar evento");
+  }
+  return (
+    <>
+      <MainContent>
+        <Container>
+          <Title titleText={"Eventos"} additionalClass="margem-acima" />
+
+          <Select
+            id="id-tipo-evento"
+            name="tipo-evento"
+            required={true}
+            object={quaisEventos}
+            mapOption={(option) => (
+              <option value={option.value} key={option.value}>
+                {option.text}
+              </option>
+            )}
+            manipulationFunction={(e) => {
+              myEvents(e.target.value);
+            }} // aqui só a variável state
+            value={tipoEvento}
+            additionalClass="select-tp-evento"
+          />
+          <Table
+            dados={tipoEvento === "1" ? eventos : meusEventos}
+            fnConnect={handleConnect}
+            fnShowModal={() => {
+              showHideModal();
+            }}
+          />
+        </Container>
+      </MainContent>
+
+      {/* SPINNER -Feito com position */}
+      {showSpinner ? <Spinner /> : null}
+
+      {showModal ? (
+        <Modal
+          userId={userData.userId}
+          showHideModal={showHideModal}
+          fnDelete={commentaryRemove}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default EventosAlunoPage;
--- JavaScript_Introducao/React/event-plus/eventplusbase/src/routes/AppRoutes.js
+++ JavaScript_Introducao/React/event-plus/eventplusbase/src/routes/AppRoutes.js
@@ -4,6 +4,7 @@ import HomePage from "../pages/HomePage/HomePage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import EventosPage from "../pages/EventosPage/EventosPage";
 import TipoEventosPage from "../pages/TipoEventosPage/TipoEventosPage";
+import EventosAlunoPage from "../pages/EventosAlunoPage/EventosAlunoPage";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import TestePage from "../pages/TestePage/TestePage";
@@ -19,7 +20,7 @@ const AppRoutes = () => {
         <Route
           element={
             <PrivateRoute redirectTo="/">
-              <EventosPage />
+              <EventosAlunoPage />
             </PrivateRoute>
           }
           path="/eventos-aluno"
